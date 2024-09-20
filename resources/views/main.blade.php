511d4c43e3cdd9130115f62ea6d511957cd40b2c
glbritoster <glbritoster@gmail.com>
2024-09-20 19:02:07 -0300

    002

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Welcome View and Blade!!</h1>
+</body>
+</html>
